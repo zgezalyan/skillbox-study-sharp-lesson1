1e7e2fe9332277c15a0697aeb5a80b05f7464ab8
Zurab Gezalyan <mcdoss@mail.ru>
2023-04-03 10:58:43 +0300

    Exercise complete

---
+++ HelloWorldConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world!!!");
+            Console.ReadKey();
+            Console.Write("Hello, ");
+            Console.Write("world");
+            Console.Write("!!!");
+            Console.ReadKey();
+        }
+    }
+}
